623de6b592b1830ace0eaffd7d38a31887d5baff
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-04 16:15:08 +0200

    fix avatar list img

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto a
 - Click sul contatto mostra la conversazione del contatto cliccato
 Milestone 3
 - Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra,
- come messaggio verde
+come messaggio verde
 - Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 Milestone 4
 - Ricerca utenti: scrivendo qualcosa nell’input a sinistra,
