2e55235cfe8a065480d3341e61942e78f33e24d7
DanieleBegnis <daniele.begnis@gmail.com>
2024-04-05 23:37:01 +0200

    feat: add milestone 4

--- js/script.js
+++ js/script.js
@@ -22,134 +22,146 @@ const { createApp } = Vue;
 const dt = luxon.DateTime
 
 createApp({
-    data() {
-        return {
-            contacts: [{
-                name: 'Michele',
-                avatar: '_1',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Hai portato a spasso il cane?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Ricordati di dargli da mangiare',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 16:15:22',
-                    message: 'Tutto fatto!',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Fabio',
-                avatar: '_2',
-                visible: true,
-                messages: [{
-                    date: '20/03/2020 16:30:00',
-                    message: 'Ciao come stai?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '20/03/2020 16:30:55',
-                    message: 'Bene grazie! Stasera ci vediamo?',
-                    status: 'received'
-                  },
-                  {
-                    date: '20/03/2020 16:35:00',
-                    message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Samuele',
-                avatar: '_3',
-                visible: true,
-                messages: [{
-                    date: '28/03/2020 10:10:40',
-                    message: 'La Marianna va in campagna',
-                    status: 'received'
-                  },
-                  {
-                    date: '28/03/2020 10:20:10',
-                    message: 'Sicuro di non aver sbagliato chat?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '28/03/2020 16:15:22',
-                    message: 'Ah scusa!',
-                    status: 'received'
-                  }
-                ],
-              },
-              {
-                name: 'Luisa',
-                avatar: '_4',
-                visible: true,
-                messages: [{
-                    date: '10/01/2020 15:30:55',
-                    message: 'Lo sai che ha aperto una nuova pizzeria?',
-                    status: 'sent'
-                  },
-                  {
-                    date: '10/01/2020 15:50:00',
-                    message: 'Si, ma preferirei andare al cinema',
-                    status: 'received'
-                  }
-                ],
-              },
-            ],
-            activeChat: 0,
-            searchContactText: "",
-            newUserMessage: "",
-            date: '',
-        };
-    },
-    methods: {
-      //funzione chat attuale
-      activateChat: function(clickedIndex) {
-        this.activeChat = clickedIndex;
-        console.log(clickedIndex)
-      },
-      //funzione che inserisce data e ora correnti
-      getCurrentDate() {
-        this.date = dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS);
-        console.log(this.date)
-      },
-      
-      //funzione che aggiunge nuovo messaggio
-      addNewMessage(activeChat) {
-        const newMessage = {
-          date: 'PLACEHOLDER',
-          message: this.newUserMessage,
+  data() {
+    return {
+      contacts: [{
+        name: 'Michele',
+        avatar: '_1',
+        visible: true,
+        messages: [{
+          date: '10/01/2020 15:30:55',
+          message: 'Hai portato a spasso il cane?',
           status: 'sent'
+        },
+        {
+          date: '10/01/2020 15:50:00',
+          message: 'Ricordati di dargli da mangiare',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 16:15:22',
+          message: 'Tutto fatto!',
+          status: 'received'
         }
-        //pusho messaggio nuovo e azzero campo inserimento
-        this.contacts[this.activeChat].messages.push(newMessage);
-        this.newUserMessage = '';
-        //risposta automatica dopo 1 secondo
-        setTimeout(() => {
-          const responseMessage = {
-            date: 'PLACEHOLDER',
-            message: 'ok',
-            status: 'received'
-          }
-          this.contacts[this.activeChat].messages.push(responseMessage)
-        },1000)
+        ],
       },
-      //funzione che cerca i contatti
-      searchContact(searchUser) {
-        const search = this.searchContactText.toLowerCase();
-        this.contacts.forEach((contact) => {
-          const name = contact.name.toLowerCase();
-          contact.visible = name.includes(search);
-        })
+      {
+        name: 'Fabio',
+        avatar: '_2',
+        visible: true,
+        messages: [{
+          date: '20/03/2020 16:30:00',
+          message: 'Ciao come stai?',
+          status: 'sent'
+        },
+        {
+          date: '20/03/2020 16:30:55',
+          message: 'Bene grazie! Stasera ci vediamo?',
+          status: 'received'
+        },
+        {
+          date: '20/03/2020 16:35:00',
+          message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+          status: 'received'
+        }
+        ],
+      },
+      {
+        name: 'Samuele',
+        avatar: '_3',
+        visible: true,
+        messages: [{
+          date: '28/03/2020 10:10:40',
+          message: 'La Marianna va in campagna',
+          status: 'received'
+        },
+        {
+          date: '28/03/2020 10:20:10',
+          message: 'Sicuro di non aver sbagliato chat?',
+          status: 'sent'
+        },
+        {
+          date: '28/03/2020 16:15:22',
+          message: 'Ah scusa!',
+          status: 'received'
+        }
+        ],
+      },
+      {
+        name: 'Luisa',
+        avatar: '_4',
+        visible: true,
+        messages: [{
+          date: '10/01/2020 15:30:55',
+          message: 'Lo sai che ha aperto una nuova pizzeria?',
+          status: 'sent'
+        },
+        {
+          date: '10/01/2020 15:50:00',
+          message: 'Si, ma preferirei andare al cinema',
+          status: 'received'
+        }
+        ],
+      },
+      ],
+      activeChat: 0,
+      searchContactText: "",
+      newUserMessage: "",
+      date: '',
+    };
+  },
+  methods: {
+    //funzione che rende attivo il contatto
+    isActive(index) {
+      if (this.currentChat === index) {
+        return 'active';
       }
-    }
+      return '';
+    },
+    //funzione chat attuale
+    activateChat: function (clickedIndex) {
+      this.activeChat = clickedIndex;
+      console.log(clickedIndex)
+    },
+    //funzione che inserisce data e ora correnti
+    getCurrentDate() {
+      this.date = dt.now().setLocale('it').toLocaleString(dt.DATETIME_SHORT_WITH_SECONDS);
+      console.log(this.date)
+    },
+
+    //funzione che aggiunge nuovo messaggio
+    addNewMessage(activeChat) {
+      const newMessage = {
+        date: 'PLACEHOLDER',
+        message: this.newUserMessage,
+        status: 'sent'
+      }
+      //pusho messaggio nuovo e azzero campo inserimento
+      this.contacts[this.activeChat].messages.push(newMessage);
+      this.newUserMessage = '';
+      //risposta automatica dopo 1 secondo
+      setTimeout(() => {
+        const responseMessage = {
+          date: 'PLACEHOLDER',
+          message: 'ok',
+          status: 'received'
+        }
+        this.contacts[this.activeChat].messages.push(responseMessage)
+      }, 1000)
+    },
+    //funzione che cerca i contatti
+    searchContact() {
+      this.contacts.forEach(contact => {
+        const search = this.searchContactText.toLowerCase();
+        console.log(search)
+        if (!(contact.name.toLowerCase().includes(this.searchContactText.toLowerCase()))) {
+          contact.visible = false;
+          console.log(contact)
+        } else {
+          contact.visible = true;
+        }
+      });
+    },
+  }
 }).mount('#app');
 getCurrentDate();
